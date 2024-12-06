18b89d2e4249f4405e968363b7e98966d358739b
gmourad <gmourad@cijun.sp.gov.br>
2024-12-06 09:31:39 -0300

    att

--- src/index.ts
+++ src/index.ts
@@ -479,27 +479,27 @@ console.log(loja1.toString())
 
 //READONLY -> posso acessar, mas não alterar fora da classe
 //private -> não pode acessar =, nem alterar fora da classe
-class Movies{
+class Movies {
     id: number
     name: string
 
     constructor(
         id: number,
         name: string,
-    ){ 
+    ) {
         this.id = id
         this.name = name
     }
 }
 
-let movie1 = new Movies(1, 'Dejavu', )
+let movie1 = new Movies(1, 'Dejavu',)
 console.log(movie1)
 
 
 //--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||
 //PARAMETRO DO CONSTRUCTOR
 //usando o mesmo codigo
-class Movies1{
+class Movies1 {
     //readonly id: number
     //name: string
 
@@ -507,13 +507,13 @@ class Movies1{
         public readonly id: number,
         public name: string,
         private _price?: number
-    ){ //usando o 'public', cria e inicializa a propriedade automatico
+    ) { //usando o 'public', cria e inicializa a propriedade automatico
         //this.id = id
         //this.name = name
     }
 }
 
-let movie11 = new Movies1(1, 'Dejavu', )
+let movie11 = new Movies1(1, 'Dejavu',)
 console.log(movie1)
 
 
@@ -528,7 +528,7 @@ let room = new hotel()
 
 room.A201 = 'Andre'
 room.A202 = 'Ana'
-room.A203 =   'Bia'                          
+room.A203 = 'Bia'
 
 console.log(room)
 
@@ -537,16 +537,16 @@ console.log(room)
 //INHERITANCE (HERENÇA)
 
 class Person1 {
-    constructor(public firstName: string, public lastName: string, public age: number){ }
+    constructor(public firstName: string, public lastName: string, public age: number) { }
 
-    get greet(){
+    get greet() {
         return this.firstName + ' ' + this.lastName
     }
 
 }
 
-class clienteBanco extends Person1{
-    override get greet(){
+class clienteBanco extends Person1 {
+    override get greet() {
         return 'Dear' + super.greet
     }
 }
@@ -556,8 +556,8 @@ class clienteBanco extends Person1{
 //OVERRIDING
 //usando o mesmo codigo
 
-class funcionarioBanco extends Person1{
-    override get greet(){
+class funcionarioBanco extends Person1 {
+    override get greet() {
         return 'Hi' + super.greet
     }
 }
@@ -567,3 +567,154 @@ let funcionario1 = new funcionarioBanco('Beatriz', 'Rizatto', 18)
 
 console.log(cliente1.greet)
 console.log(funcionario1.greet)
+
+
+//--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||
+
+// Day 7 - Exercise 1
+
+interface User10 {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin10 {
+    name: string;
+    age: number;
+    role: string;
+}
+
+type Person10 = User10 | Admin10;
+
+const persons: Person10[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut.'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver.'
+    }
+];
+
+function logPerson(person10: Person10) {
+    let additionalInformation: string;
+    if ('role' in person10) {
+        additionalInformation = person10.role;
+    } else {
+        additionalInformation = person10.occupation;
+    }
+    console.log(` - ${person10.name}, ${person10.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
+
+
+//--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||
+
+// Day 7 - Exercise 2
+
+class Animal {
+    constructor(public name: string) { }
+    move(meters: number) {
+        console.log(`${this.name} moved ${meters}m.`)
+    }
+}
+
+class Snake extends Animal {
+    override move(meters: number) {
+        console.log('Slithering...')
+    super.move(meters)
+    }
+}
+
+class Pony extends Animal {
+    override move(meters: number) {
+        console.log('Galloping...')
+    super.move(meters)
+
+    }
+}
+
+
+const sammy = new Snake("Sammy the Snake")
+sammy.move(5)
+
+const pokey = new Pony("Pokey the Pony")
+pokey.move(34)
+
+
+//--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||
+
+// Day 7 - Exercise 3
+
+class Furniture {
+    //protected readonly manufacturer: string
+
+    constructor(protected manufacturer: string = 'IKEA') {
+        this.manufacturer = manufacturer
+    }
+  }
+
+  class Desk extends Furniture {
+    kind() {
+      console.log(`This is a desk made by ${this.manufacturer}`)
+    }
+  }
+
+  class Chair extends Furniture {
+    kind() {
+      console.log(`This is a chair made by ${this.manufacturer}`)
+    }
+  }
+
+  const desk = new Desk()
+  desk.kind()
+  //desk.manufacturer // Deve retornar um erro
+
+  const chair = new Chair()
+  chair.kind()
+  //chair.manufacturer // Deve retornar um erro
+
+
+//--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||
+
+
+// Day 7 - Exercise 4
+
+let multiply: (val1: number, val2: number) => number;
+let capitalize: (val: string) => string;
+
+capitalize = function(value: string): string {
+  return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+}
+
+multiply = function(x: number, y: number): number {
+  return x * y;
+}
+
+console.log(capitalize(`nifty ${multiply(5, 10)}`));
+
+
+//--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||--||
+
+// Day 7 - Exercise 5
+
+function layEggs(quantity: number, color: string): void {
+    console.log(`You just laid ${quantity} ${color} eggs. Good job!`);
+  }
+
+  layEggs(2, 'blue');
